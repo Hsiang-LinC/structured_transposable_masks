152db022b8ace0261de984f8a778a33870389d73
ub511174 <ub511174@login23-g-1.hpc.itc.rwth-aachen.de>
2025-05-14 11:13:32 +0200

    necessary changes

--- prune/pruning_method_transposable_block_l1_graphs.py
+++ prune/pruning_method_transposable_block_l1_graphs.py
@@ -1,4 +1,9 @@
+import re
+
 import networkx as nx
+from func_timeout import FunctionTimedOut, func_timeout
+from networkx import NetworkXException
+from pulp import lpSum, LpVariable, LpProblem, LpMaximize, LpSolverDefault, LpInteger
 from tqdm import tqdm
 from multiprocessing import Pool
 from common.timer import Timer
@@ -6,9 +11,17 @@ from prune.pruning_method_utils import *
 import numpy as np
 import torch.nn.utils.prune as prune
 
+"""
+This class is taken from the structured_transposable_masks repository and is only slightly modified to combine 
+the optimal calculation of the mask using the graph and the LP formulation, since the graph approach is faster
+but does not terminate on certain instances. Hence, we start with the graph approach and if it does not terminate
+within 5 seconds, we switch to the LP formulation. 
+
+This file should replace the original file in: 
+structured_transposable_masks/prune/pruning_method_transposable_block_l1_graphs.py
+"""
 
 class PruningMethodTransposableBlockL1Graphs(prune.BasePruningMethod):
-
     PRUNING_TYPE = 'unstructured'  # pruning type "structured" refers to channels
 
     RUN_SPEED_TEST = False
@@ -28,8 +41,18 @@ class PruningMethodTransposableBlockL1Graphs(prune.BasePruningMethod):
     def nxGraph(self, data):
         bs = data.shape[0]
         G = nx.DiGraph()
-        G.add_node('s', demand=-int(bs ** 2 / 2))
-        G.add_node('t', demand=int(bs ** 2 / 2))
+        ########################################
+        # CHANGED TO SUPPORT general N:M pruning (!= 2:4)
+        ########################################
+        # G.add_node('s', demand=-int(bs ** 2 / 2))
+        # G.add_node('t', demand=int(bs ** 2 / 2))
+        # the edges in the flow are the not pruned ones (weights are -|w_ij|)
+        remaining_fraction = self.topk * bs
+        G.add_node('s', demand=-remaining_fraction)
+        G.add_node('t', demand=remaining_fraction)
+        ########################################
+        # END CHANGED
+        ########################################
         names = []
         for i in range(bs):
             G.add_edge('s', 'row' + str(i), capacity=self.topk, weight=0)
@@ -43,6 +66,7 @@ class PruningMethodTransposableBlockL1Graphs(prune.BasePruningMethod):
             mask.append(list(dictMinFLow[w].values()))
         return np.array(mask)
 
+
     def get_mask_iter(self, c):
         co, inners = self.mp_tensor.shape
         block_numel = self.bs ** 2
@@ -51,7 +75,7 @@ class PruningMethodTransposableBlockL1Graphs(prune.BasePruningMethod):
             offset = j * block_numel
             w_block = self.mp_tensor[c, offset:offset + block_numel].reshape(self.bs, self.bs)
             w_block = w_block + w_block.T if self.optimize_transposed else w_block
-            mask_block = self.nxGraph(-1 * w_block).reshape(-1) #max flow to min flow
+            mask_block = self.graph_and_lp(-1 * w_block).reshape(-1)  # max flow to min flow
             self.mp_mask[c, offset:offset + block_numel] = torch.from_numpy(mask_block)
 
     def get_mask(self, t):
@@ -66,7 +90,8 @@ class PruningMethodTransposableBlockL1Graphs(prune.BasePruningMethod):
             with Timer() as t:
                 self.get_mask_iter(0)
             elapsed = t.total().total_seconds()
-            print('Single core speed test: blocks={} secs={} block-time={}'.format(n_blocks, elapsed, elapsed/n_blocks))
+            print(
+                'Single core speed test: blocks={} secs={} block-time={}'.format(n_blocks, elapsed, elapsed / n_blocks))
 
         p = Pool(self.n_workers)
         n_iterations = co
@@ -96,3 +121,56 @@ class PruningMethodTransposableBlockL1Graphs(prune.BasePruningMethod):
         block_mask = clip_padding(mask, t_permuted.shape).reshape(t_permuted.shape)
         block_mask = permute_to_nchw(block_mask)
         return block_mask
+
+    ########################################
+    # START ADDED CODE
+    ########################################
+    def ip_transpose(self, data):
+        # copied from LP file
+        prob = LpProblem('TransposableMask', LpMaximize)
+        combinations = []
+        magnitude_loss = {}
+        indicators = {}
+        bs = self.bs
+        for r in range(bs):
+            for c in range(bs):
+                combinations.append('ind' + '_{}r_{}c'.format(r, c))
+                magnitude_loss['ind' + '_{}r_{}c'.format(r, c)] = abs(data[r, c])
+                indicators['ind' + '_{}r_{}c'.format(r, c)] = \
+                    LpVariable('ind' + '_{}r_{}c'.format(r, c), 0, 1, LpInteger)
+
+        prob += lpSum([indicators[ind] * magnitude_loss[ind] for ind in magnitude_loss.keys()])
+
+        for r in range(bs):
+            prob += lpSum([indicators[key] for key in combinations if '_{}r'.format(r) in key]) == self.topk
+        for c in range(bs):
+            prob += lpSum([indicators[key] for key in combinations if '_{}c'.format(c) in key]) == self.topk
+
+        solver = LpSolverDefault
+        solver.msg = False
+        prob.solve(solver)
+        assert prob.status != -1, 'Infeasible'
+        mask = np.zeros([self.bs, self.bs])
+        for v in prob.variables():
+            if 'ind' in v.name:
+                rc = re.findall(r'\d+', v.name)
+                mask[int(rc[0]), int(rc[1])] = v.varValue
+        return mask
+
+    def graph_and_lp(self, data):
+        try:
+            mask = func_timeout(5, self.nxGraph, args=(data,))
+        except FunctionTimedOut as e:
+            print('Timeout occured in transposable mask calculation with min. flow, switching to MIP formulation.')
+            # data is negative for min flow, LP formulation requires original magnitudes
+            mask = self.ip_transpose((-1) * data)
+        except NetworkXException as e:
+            print(
+                'Networkx exception occured in in transposable mask calculation with min. flow, switching to MIP formulation.')
+            # data is negative for min flow, LP formulation requires original magnitudes
+            mask = self.ip_transpose((-1) * data)
+        return mask
+
+    ########################################
+    # END ADDED CODE
+    ########################################
